c503337fa75f1e24d3e41b26feb034498a44676c
Matt <matthew.j.alioto@gmail.com>
2023-01-03 11:59:30 -0500

    Stubs only

--- main/antikythera.py
+++ main/antikythera.py
@@ -1,88 +1,24 @@
 from typing import List
-from rotor import Rotor
-import time
+
+class Rotor:
+    def __init__(self, contents):
+        self.contents = contents
 
 
 class Antikythera:
     num_rotors: int
     num_columns: int
-    num_layers: int
     target_sum: int
     rotors: List[Rotor]
 
-    def __init__(self, rotors: int, layers: int, columns: int, sum: int):
+    def __init__(self, rotors: int, columns: int, sum: int):
         self.rotors = []
         self.num_rotors = rotors
-        self.num_layers = layers
         self.num_columns = columns
         self.target_sum = sum
         print(
-            f"Creating a {self.num_rotors}-rotor puzzle with {self.num_columns} columns of {self.num_layers} layers of numbers that sum up to {self.target_sum}."
+            f"Creating a {self.num_rotors}-rotor puzzle with {self.num_columns} columns of numbers that sum up to {self.target_sum}."
         )
 
     def add_rotor(self, rotor: Rotor):
-        if rotor.num_columns() != self.num_columns:
-            raise Exception(
-                f"Added rotor has {rotor.num_columns()} columns but this puzzle is set for {self.num_columns}"
-            )
         self.rotors.append(rotor)
-
-    def reset_rotors(self):
-        for rotor in self.rotors:
-            rotor.reset_rotation()
-
-    def read_column(self, column_index: int) -> int:
-        column: list[int] = []
-        for layer in range(0, self.num_layers):
-            for rotor in self.rotors:
-                if not layer in rotor.rotated_contents.keys():
-                    continue
-                if rotor.rotated_contents[layer][column_index]:
-                    column.append(rotor.rotated_contents[layer][column_index])
-                    break
-        assert len(column) == self.num_layers
-        return column
-
-    def is_solved(self) -> bool:
-        if sum(self.read_column(0)) == self.target_sum:
-            for col in range(1, self.num_columns):
-                if not sum(self.read_column(col)) == self.target_sum:
-                    return False
-            return True
-        return False
-
-    def solve(self):
-        if len(self.rotors) != self.num_rotors:
-            raise Exception(
-                f"Cannot solve puzzle: {self.num_rotors} rotors specified but {len(self.rotors)} are present"
-            )
-
-        tic = time.perf_counter()
-        self._solve(rotor=self.num_rotors - 1)
-        toc = time.perf_counter()
-
-        print(
-            f"Solution found in {toc-tic:0.4f}s at permutation {self._permutations}:\n{self.to_str()}"
-        )
-
-    _permutations: int = 0
-
-    def _solve(self, rotor) -> bool:
-        self._permutations += 1
-        if self.is_solved():
-            return True
-
-        if rotor > 0:
-            for inc in range(self.num_columns):
-                self.rotors[rotor].rotate()
-                if self._solve(rotor - 1):
-                    return True
-
-        return False
-
-    def to_str(self):
-        result_str = ""
-        for col in range(self.num_columns):
-            column = self.read_column(col)
-            result_str += f"Column {col}: {column} = {sum(column)}\n"
-        return result_str
--- main/main.py
+++ main/main.py
@@ -7,45 +7,5 @@ from antikythera import Antikythera, Rotor
 # Rotors are added from the top down.
 # The objective of the puzzle is to rotate the rotors until each column adds up to 42.
 
-ant = Antikythera(5, 4, 12, 42)
-ant.add_rotor(Rotor({0: [15, None, 8, None, 3, None, 6, None, 10, None, 7, None]}))
-ant.add_rotor(
-    Rotor(
-        {
-            0: [6, 17, 7, 3, None, 6, None, 11, 11, 6, 11, None],
-            1: [12, None, 4, None, 7, 15, None, None, 14, None, 9, None],
-        }
-    )
-)
-ant.add_rotor(
-    Rotor(
-        {
-            0: [8, 9, 13, 9, 7, 13, 21, 17, 4, 5, None, 7],
-            1: [None, 21, 6, 15, 4, 9, 18, 11, 26, 14, 1, 12],
-            2: [None, 5, None, 10, None, 8, None, 22, None, 16, None, 9],
-        }
-    )
-)
-ant.add_rotor(
-    Rotor(
-        {
-            0: [7, None, 9, None, 7, 14, 11, None, 8, None, 16, None],
-            1: [9, None, 2, 3, 6, None, 14, 12, 9, None, 9, 2],
-            2: [3, 26, 6, None, 2, 13, 9, None, 17, 19, 3, 12],
-            3: [1, None, 9, None, 12, None, 6, None, 10, None, 10, None],
-        }
-    )
-)
-ant.add_rotor(
-    Rotor(
-        {
-            0: [14, 11, 14, 14, 11, 14, 11, 14, 11, 11, 14, 11],
-            1: [8, 9, 10, 11, 12, 13, 14, 15, 4, 5, 6, 7],
-            2: [3, 3, 14, 14, 21, 21, 9, 9, 4, 4, 6, 6],
-            3: [2, 5, 10, 7, 16, 8, 7, 8, 8, 3, 4, 12],
-        }
-    )
-)
-print(f"\n{ant.to_str()}")
-input("Press ENTER to continue\n")
-ant.solve()
+ant = Antikythera(5, 12, 42)
+ant.add_rotor(Rotor({}))
